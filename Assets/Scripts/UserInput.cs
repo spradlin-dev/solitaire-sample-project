5224ac316cc71da3afd432cb7c68cb5b9bb6dc31
Spradlin <github@spradlin.dev>
2026-01-18 01:45:21 -0600

    Added "New Deal" button

--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -6,21 +6,18 @@ public class UserInput : MonoBehaviour
     public GameObject selectedCard;
     private Solitaire solitaire;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         solitaire = this.gameObject.GetComponent<Solitaire>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         GetMouseClick();
-        // handle new deal with user clicks f5
+
         if (Input.GetKeyDown(KeyCode.F5))
         {
-            solitaire.PlayCards();
-            this.selectedCard = null;
+            ResetBoard();
         }
     }
 
@@ -38,7 +35,9 @@ public class UserInput : MonoBehaviour
                 {
                     GameObject clickedObject = hit.collider.gameObject;
                     GameObject parentObject = clickedObject.transform.parent?.gameObject;
-                    
+
+                    print("clicked on: " + clickedObject.name);
+
                     if (clickedObject.name == "Stock Slot")
                     {
                         HandleStockSlotClick(clickedObject);
@@ -64,23 +63,51 @@ public class UserInput : MonoBehaviour
         }
     }
 
+    public void HandleCardClick(GameObject card)
+    {
+        // TODO make cards buttons to use this function
+        GameObject parentObject = card.transform.parent?.gameObject;
+
+        print("clicked on: " + card.name);
+
+        if (parentObject && parentObject.name == "Stock Slot")
+        {
+            HandleStockClick(card);
+        }
+        else if (parentObject && parentObject.name.Contains("Ace"))
+        {
+            HandleAcesClick(card);
+        }
+        else if (parentObject && parentObject.name.Contains("Column"))
+        {
+            HandleTableauClick(card, parentObject);
+        }
+        else if (parentObject && parentObject.name == "Waste Slot")
+        {
+            HandleWasteClick(card);
+        }
+    }
+
+    public void ResetBoard()
+    {
+        solitaire.PlayCards();
+        this.selectedCard = null;
+    }
+
     void HandleStockClick(GameObject card)
     {
-        // To be implemented
         print("Stock clicked: " + card.name);
         this.selectedCard = null;
         solitaire.Deal3ToWaste();
     }
     void HandleStockSlotClick(GameObject stockSlot)
     {
-        // To be implemented
         print("Stock Slot clicked: " + stockSlot.name);
         this.selectedCard = null;
         solitaire.ResetStockFromWaste();
     }
     void HandleAcesClick(GameObject card)
     {
-        // To be implemented
         print("Aces clicked: " + card.name);
         if (!card.GetComponent<Selectable>().isTopCardInContainer)
         {
@@ -102,20 +129,22 @@ public class UserInput : MonoBehaviour
         if (card.GetComponent<Selectable>().isFaceUp == false)
         {
             this.selectedCard = null;
-            return;
         }
-        if (this.selectedCard == card && card.GetComponent<Selectable>().isTopCardInContainer)
+        else if (this.selectedCard == card && card.GetComponent<Selectable>().isTopCardInContainer)
         {
             this.selectedCard = null;
             this.solitaire.AttemptToPlayCard(card);
-            return;
-        } else if (this.selectedCard == card && !card.GetComponent<Selectable>().isTopCardInContainer)
+        }
+        else if (this.selectedCard == card && !card.GetComponent<Selectable>().isTopCardInContainer)
         {
             this.selectedCard = null;
             this.solitaire.AttemptToPlayCardStack(card);
-            return;
+        } 
+        else
+        {
+            this.selectedCard = card;
         }
-        this.selectedCard = card;
+            
     }
 
     void HandleWasteClick(GameObject card)
@@ -123,15 +152,16 @@ public class UserInput : MonoBehaviour
         if (!card.GetComponent<Selectable>().isTopCardInContainer)
         {
             this.selectedCard = null;
-            return;
-        }
-        if (this.selectedCard == card)
+        } 
+        else if (this.selectedCard == card)
         {
             this.selectedCard = null;
             this.solitaire.AttemptToPlayCard(card);
-            return;
+        } 
+        else
+        {
+            this.selectedCard = card;
         }
-        this.selectedCard = card;
     }
 
 }
